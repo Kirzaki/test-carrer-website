e4a85f5ebf5b55d98fc496ed6ecdcf786d9119bb
Kirzaki <chakir.z@outlook.com>
2024-01-15 10:58:39 -0500

    First draft of the website Just finished the html version of the home page

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+# 41 : 02
+
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('home.html')
+
+# Adding the page icon
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+        'favicon.ico')
 
 if __name__ == "__main__" :
     app.run(host = '0.0.0.0', debug = True)
