c114581e0f12a36e328eead9b974a8906563bcae
Kirzaki <chakir.z@outlook.com>
2024-01-27 14:54:42 -0500

    Small debugging

--- app.py
+++ app.py
@@ -60,3 +60,8 @@ def list_jobs():
 
 if __name__ == "__main__" :
     app.run(host = '0.0.0.0', debug = True)
+
+
+@app.route("/offers")
+def home():
+    return render_template('home.html', tutors = TUTORS)
